5f11f813dfff22272804c5b62738bd674dac69e8
Shubham Kumar <shubham.kumar@aicrowd.com>
2024-09-24 12:13:02 +0530

    Added utils for error and response

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
-
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+    app.on("error", (error) => {
+      console.log("ERROR:", error);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed!!", err);
+  });
 
+// Alternate way to connect db & express using IFFE function
 /* 
 import express from "express";
 const app = express();
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// async handler wrapper using pomise
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// async handler wrapper using try catch
+/* 
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error,
+    });
+  }
+};
+*/
